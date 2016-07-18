e5af7e8ab382db6b0ab49097e64b4c61f9eca5ab
Nadia Pillard <ua9ond@yandex.ru>
2016-07-18 20:20:22 +0600

    form validation

--- js/main.js
+++ js/main.js
@@ -5,8 +5,10 @@ $(document).ready(function(e) {
     $('.popup-report').after("<div class='popup-report__overlay'></div>");
     $('.popup-report, .popup-report__overlay').hide();
     $('.basic-btn').click(function(evt){
+      
       evt.preventDefault();
       $('.popup-report, .popup-report__overlay').fadeIn(500);
+      $('.popup-report #report-name').focus();
     })
     $('.popup-report__close, .popup-report__overlay').click(function(){
       $('.popup-report, .popup-report__overlay').fadeOut(500);
@@ -17,9 +19,69 @@ $(document).ready(function(e) {
         $('.popup-report, .popup-report__overlay').fadeOut(500);
     });
     
-    $('#input-phone').inputmask("+7 (999) 999-9999");
+    var validValue = 0; 
+    $('#input-phone').inputmask("+7 (999) 999-9999", 
+      { "onincomplete": function() {
+        validValue = 0;
+        $(this).addClass('call-master__input--invalid');
+        showErr('input-phone', 'Введите номер полностью');
+        },
+        "oncomplete": function() {
+        validValue = 2;
+        $(this).removeClass('call-master__input--invalid');
+        console.log(validValue);
+        removeErr('input-phone');
+        },
+       "onKeyValidation": function() {
+         removeErr('input-phone');
+       }
+      }                         
+    );
+  
+    $('#input-name').inputmask('Regex', { regex: "[A-Za-zА-Яа-яЁё]{3-20}",
+                                        "onincomplete": function() {
+        validValue = 1;
+        $(this).addClass('call-master__input--invalid');
+        showErr('input-phone', 'Введите номер полностью');
+        }
+    });
+  
+    $('.call-master__btn').click(function(evt){
+      if(validValue === 0) {
+        evt.preventDefault();
+        $('#input-phone').addClass('call-master__input--invalid');
+      
+        if (!$("label[for='input-phone']").children().hasClass("call-master__errorMsg")){
+          showErr('input-phone', 'Введите номер телефона');
+        }
+      }
+    });
+  
+    function showErr(field, errorMessage) {
+      var errorSpan = document.createElement("span");
+      var errorMessage = document.createTextNode(errorMessage);
+
+      errorSpan.appendChild(errorMessage);
+      errorSpan.className = "call-master__errorMsg";
+
+      var $fieldLabel = $("label[for='" + field + "']");
+      $fieldLabel.append(errorSpan);
+    }
+  
+    function removeErr(field) {
+      var $fieldLabel = $("label[for='" + field + "']");
+      
+      if ($fieldLabel.children().hasClass("call-master__errorMsg")){
+        $fieldLabel.find('.call-master__errorMsg').remove();
+      }
+    }
+  
+    
+  
+    
     $('#report-phone').inputmask("+7 (999) 999-9999")
-    $('#input-name').inputmask('Regex', { regex: "[A-Za-zА-Яа-яЁё]{3-20}" });
+    
     $('#report-name').inputmask('Regex', { regex: "[A-Za-zА-Яа-яЁё]{3-20}" });
    
 });
+
