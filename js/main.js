0ddaf8e9f3c2111a3b2141b2dfc810d6e093e9be
Nadia Pillard <ua9ond@yandex.ru>
2016-07-18 12:28:20 +0600

    popup function cahnged

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 
 "use strict";
 $(document).ready(function(e) {
+    $('.popup-report').after("<div class='popup-report__overlay'></div>");
     $('.popup-report, .popup-report__overlay').hide();
     $('.basic-btn').click(function(evt){
       evt.preventDefault();
@@ -9,6 +10,11 @@ $(document).ready(function(e) {
     $('.popup-report__close, .popup-report__overlay').click(function(){
       $('.popup-report, .popup-report__overlay').fadeOut(500);
     })
+    
+    $(this).keydown(function(eventObject){
+      if (eventObject.which == 27)
+        $('.popup-report, .popup-report__overlay').fadeOut(500);
+    });
     
     $('#input-phone').inputmask("+7 (999) 999-9999");
     $('#report-phone').inputmask("+7 (999) 999-9999")
