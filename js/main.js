3cfee4a0a730258fd177a420de5067377fda687e
Nadia Pillard <ua9ond@yandex.ru>
2016-07-18 23:49:29 +0600

    animation added

--- js/main.js
+++ js/main.js
@@ -19,11 +19,17 @@ $(document).ready(function(e) {
   //закрытие попапа при нажатии на крестик или по затемненному фону
   $('.popup-report__close, .popup-report__overlay').click(function(){
     $('.popup-report, .popup-report__overlay').fadeOut(500);
+    if ($('.popup-report').hasClass('popup-report__shake')) {
+      $('.popup-report').removeClass('popup-report__shake');
+    }
   })
   //закрытие попапа при нажатии на клавишу esc  
   $(this).keydown(function(eventObject){
     if (eventObject.which == 27)
       $('.popup-report, .popup-report__overlay').fadeOut(500);
+      if ($('.popup-report').hasClass('popup-report__shake')) {
+        $('.popup-report').removeClass('popup-report__shake');
+      }
   });
     
   
@@ -166,7 +172,8 @@ $(document).ready(function(e) {
     });
   
   $('.popup-report__btn').click(function(evt){
-      if(popupValidPhone === 0) {
+      
+      if (popupValidPhone === 0) {
         evt.preventDefault();
         $('#report-phone').addClass('popup-report__input--invalid');
       
@@ -175,12 +182,19 @@ $(document).ready(function(e) {
         }
       }
       
-      if(popupValidName === 0) {
+      if (popupValidName === 0) {
         evt.preventDefault();
         $('#report-name').addClass('popup-report__input--invalid');
         if (!$("label[for='input-name']").children().hasClass("popup-report__error")){
            showErr('report-name', 'popup-report__error');
         }
+      }
+    
+      if (popupValidName === 0 || popupValidPhone === 0) {
+        if ($('.popup-report').hasClass('popup-report__shake')) {
+          $('.popup-report').removeClass('popup-report__shake');
+        }
+        $('.popup-report').addClass('popup-report__shake');
       }
     });
   
